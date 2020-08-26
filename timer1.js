6a4b7fd1dc5d56e3a71fbac6dded12ef0e06fb8e
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-25 19:03:33 -0600

    timer

--- timer1.js
+++ timer1.js
@@ -3,14 +3,7 @@ console.clear();
 // make a timer which will beep after a set time has passed. 
 // can set unlimited number of alarms using command line args 
 
-// function that accepts commandline args //
-// will need to use .argv -- process.argc.slice(2) -- this turns the input into a string!!!! 
 
-// func block should have //
-// arrow function syntax? -- timer() => { statements }
-// count down timer where the set start is cml input -- 
-// an beep when timer ends.
-// for a beep/ding ---  process.stdout.write('\x07');  --- \x07 is a special charactor code 
 
 // edge cases? -- what happens if? // 
 
