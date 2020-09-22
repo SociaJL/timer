0e735f2d425639baaaa5f6cae13e509fe259c9f5
SociaJL <53660306+SociaJL@users.noreply.github.com>
2020-09-22 14:50:57 -0600

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,24 +1,15 @@
 console.clear();
-
-// make a timer which will beep after a set time has passed. 
-// can set unlimited number of alarms using command line args 
-
-
-
-// edge cases? -- what happens if? // 
-
-// no input? -- no beeps and program ends -- if statement?
-// input is a negitive number? -- console.log(we cant schedule anything in the past) -- if statement?
-// an input is not a number? -- skip these insead of trying to call setTimeout with a non-number. if statement?
-
+// variable that is = to the input from the user on cli
 const timeSet = process.argv.slice(2);
 
+// if the num entered is less than or equaL to 0, return "cant travel back in time"
 for (let i = 0; i < timeSet.length; i++) {
-  console.log(Number(timeSet[i]));
+  //console.log(Number(timeSet[i]));
   if (Number(timeSet[i]) <= 0) {
     console.log("cant travel back in time");
     timeSet.splice(i, 1);
   }
+  // if the input on the cli is not a number, return "Not a number"
   if (Number.isNaN(Number(timeSet[i]))) {
   //if (!Number.isInteger(timeSet[i])) {
     console.log("Not a number");
@@ -26,16 +17,20 @@ for (let i = 0; i < timeSet.length; i++) {
   }
 }
 
+// timer function that iterats 1x per second (1000ms)
 const timer = function () {
-  console.log(timeSet)
+  //console.log(timeSet)
+  // starts count at 0
   let totalSeconds = 0;
+  //loops through the numbers entered by user and sets totalSeconds to the number entered on cli
   for (let seconds of timeSet) {
     totalSeconds += Number(seconds);
+    // setTimeout prints 'ding' to screen after waiting the value of totalseconds * 1 sec (1000ms)
     setTimeout(() => console.log('ding'),
     totalSeconds * 1000); 
   }
 };
-
+// calls the timer function
 timer();
 
 
